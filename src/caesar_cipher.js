9efe8a6326e60bcc6507d79770932c1916769c9a
Ryan Gerry <gerryster@gmail.com>
2020-03-29 15:26:35 -0500

    converted letters into numbers

--- src/caesar_cipher.js
+++ src/caesar_cipher.js
@@ -4,15 +4,11 @@ function CaesarCipher() {
 }
 
 CaesarCipher.prototype.encrypt = function(msg) {
-  if(msg === 'a') {
-    return 'x';
-  } else if(msg === 'b') {
-    return 'y';
-  }if (msg === 'c') {
-    return 'z';
-  }if (msg === 'z') {
-    return 'w';
+  var modifier = -3;
+  if(msg != 'z') {
+    modifier = 23;
   }
+  return String.fromCharCode(msg.codePointAt(0) + modifier);
 }
 
 module.exports = CaesarCipher;
